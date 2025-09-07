e764d38651de6a82bc284532c7d6ca538f71300d
Yehuda Jacobs <yehudaj@gmail.com>
2025-09-07 14:44:15 -0400

    Convert to Flask web application

--- main.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Hello World! Welcome to MCON 357!")
-
-if __name__ == "__main__":
-    main()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello World! Welcome to MCON 357!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
